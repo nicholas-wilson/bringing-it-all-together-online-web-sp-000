3398a0efec494c6ba0a00e2bde363bf1921e447f
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-22 04:02:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,6 +14,7 @@ class Dog
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
   end
 
   def self.create_table
