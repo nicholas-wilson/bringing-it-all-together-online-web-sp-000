633af46cb3f4e024f228a8a884e6bd1d1f004369
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-22 04:12:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,17 @@ class Dog
     self.create(attributes)
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
